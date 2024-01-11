6719d3dbc15536fd39982f5becd41a1e290f2b75
Shashank-Mahawar <imshashank296@gmail.com>
2024-01-11 23:20:02 +0530

    Loading Spinner Fixed : Align Center

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,7 +1,9 @@
 export function Loader() {
   return (
-    <div className="spinner-border" role="status">
-      <span className="visually-hidden">Loading...</span>
+    <div className="d-flex justify-content-center">
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
     </div>
   );
 }
